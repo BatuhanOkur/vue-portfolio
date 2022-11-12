155400282f251e41c164cea97e43fac332be9b7e
Batuhan Okur <batuhanokur2209@gmail.com>
2022-11-12 12:34:31 +0300

    MainCard.vue has been created

---
+++ portfolio/src/components/MainCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="main-container">
+        <div class="container-one">
+            <div class="profile">
+                <img src="../assets/profilepic.jpg" alt="">
+                <h3>Name Here</h3>
+                <p>Software Developer</p>
+            </div>
+            <div class="social-links">
+                <a href="">
+                    <div class="link">
+                        <img src="" alt="">
+                        <h2>GitHub</h2>
+                    </div>
+                    <div class="link">
+                        <img src="" alt="">
+                        <h2>LinkedIn</h2>
+                    </div>
+                    <div class="link">
+                        <img src="" alt="">
+                        <h2>Hackerrank</h2>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+
+</style>
